1f8b3f553cf484e99e7a4a12da9e6350bf62319d
zetako <zetako@outlook.com>
2019-11-19 15:35:11 +0800

    testing control

--- design/control_test.v
+++ design/control_test.v
@@ -25,3 +25,38 @@ module mainCtl_test();
 
 
 endmodule //主控电路测试
+
+module ALUctl_test();
+    reg [2:0] ALUop;
+    reg [5:0] funct;
+    wire [2:0] ALUctr;
+
+    initial
+    begin
+        ALUop=3'b000;
+        funct=4'b0000;
+    end
+
+    always #10
+    begin
+        if (ALUop==3'b000) ALUop=3'b010;
+        else if (ALUop==3'b010) ALUop=3'b001;
+        else if (ALUop==3'b001) ALUop=3'b011;
+        else if (ALUop==3'b011) ALUop=3'b100;
+        else if (ALUop==3'b100)
+        begin
+            if (funct==4'b0000) funct=4'b0010;
+            else if (funct==4'b0010) funct=4'b0100;
+            else if (funct==4'b0100) funct=4'b0101;
+            else if (funct==4'b0101) funct=4'b1010;
+            else if (funct==4'b1010)
+            begin
+                funct=4'b0000;
+                ALUop=3'b000;
+            end
+        end
+    end
+
+    ALUControl _ALU(.ALUop(ALUop),.funct(funct),.ALUctr(ALUctr));
+
+endmodule // ALU控制测试
