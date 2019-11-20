d25f524c48225acb5a28f96c9aad2952cece1590
zetako <zetako@outlook.com>
2019-11-20 15:53:30 +0800

    Many things need reconstruction!

---
+++ design/mux.v
@@ -0,0 +1,14 @@
+module mux #(parameter width=1) (select,in0,in1,out);
+    input [width-1:0] in0,in1;
+    input select;
+    output [width-1:0] out;
+
+    reg [width-1:0] out;
+
+    always @(select)
+    begin
+        if (select) out=in1;
+        else out=in0;    
+    end
+
+endmodule // 选择器
