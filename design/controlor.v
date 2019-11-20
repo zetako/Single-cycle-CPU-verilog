914622d110fe18d981cd86c4f8a94f8dc1bd9938
zetako <zetako@outlook.com>
2019-11-20 20:39:50 +0800

    restruction

--- design/controlor.v
+++ design/controlor.v
@@ -1,15 +1,5 @@
-module controlor(OP,funct,jump,extop,branch,memWrite,
-memToReg,ALUsrc,regWrite,regDst,ALUctr);
-    input [5:0] OP;
-    input [5:0] funct;
-    output jump,extop,branch;
-    output memWrite,memToReg,ALUsrc;
-    output regWrite,regDst;
-    output [2:0] ALUctr;
+module controlor(
+    
+);
 
-    wire [2:0] ALUop;
-
-    main_control main(.OP(OP),.jump(jump),.extop(extop),.branch(branch),.memWrite(memWrite),.memToReg(memToReg),.ALUsrc(ALUsrc),.regWrite(regWrite),.regDst(regDst),.ALUop(ALUop));
-    ALU_control ALU(.ALUop(ALUop),.funct(funct),.ALUctr(ALUctr));
-
-endmodule
+endmodule // 控制器
